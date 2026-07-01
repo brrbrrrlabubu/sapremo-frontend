f2d16fdfda42d3b228a8b216bb3e32763d818a6d
limon <bdljvvdvxv30m@gmail.com>
2026-07-01 10:57:23 +0600

    fix: use relative base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/sapremo-frontend/",
+  // Измени на "./" - это заставит Vite искать файлы в той же папке, где лежит index.html
+  base: "./", 
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
 })
