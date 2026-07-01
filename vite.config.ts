b5768b2561d45a17eb0f90ccb18db21b8417d95b
limon <bdljvvdvxv30m@gmail.com>
2026-07-01 11:06:53 +0600

    build: remove dist from gitignore and add production files

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Измени на "./" - это заставит Vite искать файлы в той же папке, где лежит index.html
-  base: "./", 
+  base: "./",
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  }
 })
